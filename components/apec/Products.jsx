f475be56214418eb718fa6c5e634167d9ae3e8b4
Abdullatif <abdullatifk135@gmail.com>
2023-07-26 23:17:47 +0300

    build product page

--- components/apec/Products.jsx
+++ components/apec/Products.jsx
@@ -5,6 +5,39 @@ import BottomSec from "../UI/BottomSec";
 import apecWhite from "../../public/assets/svg/apeWhite.svg";
 import backSpaceRed from "../../public/assets/jpg&png/backSpaceWhite.png";
 import { useRouter } from "next/router";
+const DUMMY_DATA = [
+  {
+    id: "ewrew324234",
+    name: "WINDSCREEN WIPERS",
+    description: "Part synthetic engine oil, 1L",
+    imgUrl: "",
+    price: 5,
+  },
+  {
+    id: "ewrewdsf324234",
+    name: "CASTROL LUBES",
+    description: "Part synthetic engine oil, 1L",
+    types: "5W-30",
+    imgUrl: "",
+    price: 5,
+  },
+  {
+    id: "ewrew3asdf24234",
+    name: "CASTROL LUBES",
+    description: "Part synthetic engine oil, 1L",
+    types: "5W-40",
+    imgUrl: "",
+    price: 5,
+  },
+  {
+    id: "ewrew3asdf24234",
+    name: "WINDSCREEN WIPERS",
+    description: "Part synthetic engine oil, 1L",
+    types: "10W-40",
+    imgUrl: "",
+    price: 5,
+  },
+];
 const Products = ({ backToHome }) => {
   const router = useRouter();
   return (
@@ -24,8 +57,8 @@ const Products = ({ backToHome }) => {
           router.push("/");
         }}
       />
-      <div className="servicesBody">
-        <h2>Products</h2>
+      <div className="productBody servicesBody">
+        <h2>PRODUCTS</h2>
       </div>
       <BottomSec />
     </div>
