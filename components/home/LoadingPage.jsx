80d8a643b3ebc5786a6ac5bb67b1edb5ff42cae3
Abdullatif <abdullatifk135@gmail.com>
2023-07-26 19:37:41 +0300

    fixed loading page

--- components/home/LoadingPage.jsx
+++ components/home/LoadingPage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Image from "next/image";
 import apecWhite from "../../public/assets/svg/apeWhite.svg";
+import doted from "../../public/assets/jpg&png/doted.png";
 const LoadingPage = () => {
   return (
     <div className="loadingMain">
+      <Image src={doted} width={250} height={200} className="doted" />
       <div className="bodyContainer">
         <div className="textContainer">
           <h2 className="animationText">
